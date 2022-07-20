503ca85496cb1e3f28c51d856c3fb7b1c300e566
Bartłomiej Kiliszek <bartekkiliszek@gmail.com>
2022-07-21 00:05:55 +0200

    feat: Add Jason Converter class

---
+++ src/main/java/pl/henkil/battleship/server/utils/JsonConverter.java
@@ -0,0 +1,10 @@
+package pl.henkil.battleship.server.utils;
+
+public class JsonConverter {
+    private JsonConverter() {
+    }
+
+    public static <T> T from(String message, Class<T> classType) {
+        return null;
+    }
+}
